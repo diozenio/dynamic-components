fb891224bdd18e568dbc44536b3ac40a4a86a4a2
diozenio <d.dionisiobsl@gmail.com>
2023-05-24 23:08:52 -0300

    Add Modal component

--- src/components.js
+++ src/components.js
@@ -24,3 +24,36 @@ export function Input(id, placeholder, labelText) {
   return inputWrapper;
 }
 
+export function Modal(title, formContent, onSubmit) {
+  const modalBackground = document.createElement("div");
+  modalBackground.className = "modalBackground";
+
+  const form = document.createElement("form");
+  form.className = "modal-content";
+  form.onsubmit = onSubmit;
+
+  for (let i = 0; i < formContent.length; i++) {
+    form.appendChild(formContent[i]);
+  }
+
+  const modal = document.createElement("div");
+  modal.className = "modal";
+  if (!!title) {
+    modal.innerHTML = `
+    <div class="modal-header">
+      <span class="modal-title">${title}</span>
+      <span class="modal-close">&times;</span>
+    </div>
+`;
+
+    const modalClose = modal.querySelector(".modal-close");
+    modalClose.addEventListener("click", () => {
+      modalBackground.remove();
+    });
+  }
+
+  modal.appendChild(form);
+
+  modalBackground.appendChild(modal);
+  return modalBackground;
+}
