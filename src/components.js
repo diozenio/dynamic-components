f98901968b3371c2f281f9788ead6ebc07f87694
diozenio <d.dionisiobsl@gmail.com>
2023-05-24 23:08:39 -0300

    Add Input component

--- src/components.js
+++ src/components.js
@@ -6,3 +6,21 @@ export function Button(buttonText, onClickFunction) {
   return button;
 }
 
+export function Input(id, placeholder, labelText) {
+  const input = document.createElement("input");
+  input.placeholder = placeholder ? placeholder : "";
+  input.className = "input";
+  input.id = id;
+  input.required = true;
+  const inputWrapper = document.createElement("div");
+  inputWrapper.className = "inputWrapper";
+  if (labelText) {
+    const label = document.createElement("label");
+    label.htmlFor = id;
+    label.innerText = labelText;
+    inputWrapper.appendChild(label);
+  }
+  inputWrapper.appendChild(input);
+  return inputWrapper;
+}
+
